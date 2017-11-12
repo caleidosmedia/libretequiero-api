38118e9f2293c1d3fde12048730cefcafa6551f0
Antonio <aespinoza@caleidos.pe>
2017-11-12 00:55:45 -0500

    denuncia

--- database/seeds/GruposTableSeeder.php
+++ database/seeds/GruposTableSeeder.php
@@ -12,16 +12,16 @@ class GruposTableSeeder extends Seeder
      */
     public function run()
     {
-        Grupo::create(['clave' => 'Ranas','valor' => 'ranas','clase_id' => 1]);
-        Grupo::create(['clave' => 'Loros grandes y guacamayos','valor' => 'loros-grandes-y-guacamayos','clase_id' => 2]);
-        Grupo::create(['clave' => 'Loros medianos y pequenos','valor' => 'loros-medianos-y-pequenos','clase_id' => 2]);
-        Grupo::create(['clave' => 'Coaties','valor' => 'coaties','clase_id' => 3]);
-        Grupo::create(['clave' => 'Felinos menores','valor' => 'felinos-menores','clase_id' => 3]);
-        Grupo::create(['clave' => 'Perezosos','valor' => 'perezosos','clase_id' => 3]);
-        Grupo::create(['clave' => 'Primates pequenos','valor' => 'primates-pequenos','clase_id' => 3]);
-        Grupo::create(['clave' => 'Primates grandes y medianos','valor' => 'primtes-grandes-y-medianos','clase_id' => 3]);
-        Grupo::create(['clave' => 'Caimanes','valor' => 'caimanes','clase_id' => 4]);
-        Grupo::create(['clave' => 'Tortugas de rio','valor' => 'tortugas-de-rio','clase_id' => 4]);
-        Grupo::create(['clave' => 'Tortugas terrestres','valor' => 'tortugas-terrestres','clase_id' => 4]);
+        Grupo::create(['clave' => 'Ranas','valor' => 'ranas','clase' => 'AMPHIBIA']);
+        Grupo::create(['clave' => 'Loros grandes y guacamayos','valor' => 'loros-grandes-y-guacamayos','clase' => 'AVES']);
+        Grupo::create(['clave' => 'Loros medianos y pequenos','valor' => 'loros-medianos-y-pequenos','clase' => 'AVES']);
+        Grupo::create(['clave' => 'Coaties','valor' => 'coaties','clase' => 'MAMMALIA']);
+        Grupo::create(['clave' => 'Felinos menores','valor' => 'felinos-menores','clase' => 'MAMMALIA']);
+        Grupo::create(['clave' => 'Perezosos','valor' => 'perezosos','clase' => 'MAMMALIA']);
+        Grupo::create(['clave' => 'Primates pequenos','valor' => 'primates-pequenos','clase' => 'MAMMALIA']);
+        Grupo::create(['clave' => 'Primates grandes y medianos','valor' => 'primtes-grandes-y-medianos','clase' => 'MAMMALIA']);
+        Grupo::create(['clave' => 'Caimanes','valor' => 'caimanes','clase' => 'REPTILIA']);
+        Grupo::create(['clave' => 'Tortugas de rio','valor' => 'tortugas-de-rio','clase' => 'REPTILIA']);
+        Grupo::create(['clave' => 'Tortugas terrestres','valor' => 'tortugas-terrestres','clase' => 'REPTILIA']);
     }
 }
