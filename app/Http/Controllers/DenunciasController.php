26a227f89e653ae49c1693c33c6fdda0eec00a29
Antonio <aespinoza@caleidos.pe>
2017-11-12 08:26:50 -0500

    denuncia

--- app/Http/Controllers/DenunciasController.php
+++ app/Http/Controllers/DenunciasController.php
@@ -134,35 +134,32 @@ class DenunciasController extends Controller
             if($data["result"]["metadata"]["intentName"] == "denuncia"){
               if($data["result"]["actionIncomplete"] != 1)
               {
+                  $tipo_locacion = $data["result"]["parameters"]["tipo_locacion"][0];
                  $clase = $data["result"]["parameters"]["clase"][0];
                  $grupo = $data["result"]["parameters"]["grupo"][0];
-                 $color = $data["result"]["parameters"]["color"][0];
-                 $animals = Animal::where('class',$clase)
-                 ->where('grupo',$grupo)
-                 ->where('color',$color)
-                 ->get();
-                 $respond = 'Ningun animal encontrado';
-                 if(!$animals->isEmpty())
-                  {
-                    $dataResponse['facebook'] = array();
-                    $dataResponse['facebook']['attachment'] = array();
-                    $dataResponse['facebook']['attachment']['type'] = 'template';
-                    $dataResponse['facebook']['attachment']['payload'] = array();
-                    $dataResponse['facebook']['attachment']['payload']['template_type'] = 'generic';
-                    $dataResponse['facebook']['attachment']['payload']['elements'] = array();
-                    foreach ($animals as $value) {
-                      $opcion = array();
-                      $opcion['title'] = $value->common_name;
-                      $opcion['image_url'] = $value->image_url;
-                      $opcion['buttons'] = array();
-                      $boton = array();
-                      $boton['type'] = 'web_url';
-                      $boton['title'] = 'Saber mas';
-                      $boton['url'] = 'https://es.wikipedia.org/wiki/'.$value->scientific_name;
-                      $opcion['buttons'][] = $boton;
-                      $dataResponse['facebook']['attachment']['payload']['elements'][] = $opcion;
-                    }
-                 }
+                 $estado = $data["result"]["parameters"]["estado"][0];
+                 $almacenamiento = $data["result"]["parameters"]["almacenamiento"][0];
+                 $descripcion = $data["result"]["parameters"]["descripcion"][0];
+                 $atributos = array(
+                  'tipo_locacion' => $tipo_locacion,
+                  'clase' => $clase,
+                  'grupo' => $grupo,
+                  'estado' => $estado,
+                  'almacenamiento' => $almacenamiento,
+                  'descripcion' => $descripcion,
+                  'coordenadas' => '-12,-77',
+                  'direccion' => 'pucp'
+                 );
+                  $denuncia = Denuncia::create($atributos);
+                  if ($denuncia) {
+                      /*$tokens = User::where('type_document', 2)->pluck('device')->toArray();
+                      if ($tokens) {
+                          $title = 'Robo vehiculo';
+                          $msg = $request->placa;
+                          $this->pushService->send($tokens, $title, $msg);
+                      }*/
+                      $respond = 'Denuncia recibida';
+                  }
               } else {
               switch ($data["result"]["fulfillment"]["speech"]) {
                   case "Que tipo":
@@ -186,6 +183,27 @@ class DenunciasController extends Controller
                       $opcion['payload'] = 'Otro';
                       $dataResponse['facebook']['quick_replies'][] = $opcion;
                       break;
+                  case "Que clase":
+                      $respond = "clase";
+                      $dataResponse['facebook'] = array();
+                      $dataResponse['facebook']['attachment'] = array();
+                      $dataResponse['facebook']['attachment']['type'] = 'template';
+                      $dataResponse['facebook']['attachment']['payload'] = array();
+                      $dataResponse['facebook']['attachment']['payload']['template_type'] = 'generic';
+                      $dataResponse['facebook']['attachment']['payload']['elements'] = array();
+                      foreach (Clase::all() as $value) {
+                        $opcion = array();
+                        $opcion['title'] = $value->clave;
+                        $opcion['image_url'] = 'http://clientes.caleidosmedia.com/dejaloslibres-api/public/img/'.$value->clave.'.png';
+                        $opcion['buttons'] = array();
+                        $boton = array();
+                        $boton['type'] = 'postback';
+                        $boton['title'] = 'Seleccionar';
+                        $boton['payload'] = $value->clave;
+                        $opcion['buttons'][] = $boton;
+                        $dataResponse['facebook']['attachment']['payload']['elements'][] = $opcion;
+                      }
+                      break;
                   case "Que grupo":
                       $respond = "grupo";
                       $dataResponse['facebook'] = array();
@@ -203,23 +221,67 @@ class DenunciasController extends Controller
                         $boton = array();
                         $boton['type'] = 'postback';
                         $boton['title'] = 'Seleccionar';
-                        $boton['payload'] = $value->valor;
+                        $boton['payload'] = $value->clave;
                         $opcion['buttons'][] = $boton;
                         $dataResponse['facebook']['attachment']['payload']['elements'][] = $opcion;
                       }
                       break;
-                  case "Que color":
-                      $respond = "clase";
+                  case "Que estado":
+                      $respond = "estado";
                       $dataResponse['facebook'] = array();
-                      $dataResponse['facebook']['text'] = 'De que color es?';
+                      $dataResponse['facebook']['text'] = 'Tipo de lugar?';
                       $dataResponse['facebook']['quick_replies'] = array();
-                      foreach (Color::all() as $value) {
-                        $opcion = array();
-                        $opcion['content_type'] = 'text';
-                        $opcion['title'] = $value->clave;
-                        $opcion['payload'] = $value->valor;
-                        $dataResponse['facebook']['quick_replies'][] = $opcion;
-                      }
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Vivo';
+                      $opcion['payload'] = 'Vivo';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Vivo - Mal Herido';
+                      $opcion['payload'] = 'Vivo - Mal Herido';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Vivo - Enfermo';
+                      $opcion['payload'] = 'Vivo - Enfermo';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Muerto';
+                      $opcion['payload'] = 'Muerto';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Muerto - En forma de producto';
+                      $opcion['payload'] = 'Muerto - En forma de producto';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      break;
+                  case "Que almacenamiento":
+                      $respond = "almacenamiento";
+                      $dataResponse['facebook'] = array();
+                      $dataResponse['facebook']['text'] = 'Tipo de lugar?';
+                      $dataResponse['facebook']['quick_replies'] = array();
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Jaula';
+                      $opcion['payload'] = 'Jaula';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Cajas';
+                      $opcion['payload'] = 'Cajas';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Bolsas'
+                      $opcion['payload'] = 'Bolsas';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
+                      $opcion = array();
+                      $opcion['content_type'] = 'text';
+                      $opcion['title'] = 'Otro';
+                      $opcion['payload'] = 'Otro';
+                      $dataResponse['facebook']['quick_replies'][] = $opcion;
                       break;
                   default:
                       $respond = $data["result"]["fulfillment"]["speech"];
