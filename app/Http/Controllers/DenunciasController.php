7ae4552f3bbd4debc5e6e87e193de89c9a008b89
Antonio <aespinoza@caleidos.pe>
2017-11-11 23:27:14 -0500

    webhook

--- app/Http/Controllers/DenunciasController.php
+++ app/Http/Controllers/DenunciasController.php
@@ -4,26 +4,78 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Denuncia;
+use Illuminate\Support\Facades\Log;
 
 class DenunciasController extends Controller
 {
     public function store(Request $request)
     {
         try {
-            $atributos = $request->all();
-            $denuncia = Denuncia::create($atributos);
+            $data = $request->all();
+            Log::info(print_r($data,true));
+          $dataResponse = array();
+           if($data["result"]["metadata"]["intentName"] == "Default Welcome Intent"){
+              switch ($data["result"]["fulfillment"]["speech"]) {
+                  case "Que grupo":
+                      $respond = "grupo";
+                      $dataResponse['facebook'] = array();
+                      $lorem = array();
+                      $lorem['content_type'] = 'text';
+                      $lorem['title'] = 'lorem';
+                      $lorem['payload'] = 'lorem';
+                      $ipsum = array();
+                      $ipsum['content_type'] = 'text';
+                      $ipsum['title'] = 'ipsum';
+                      $ipsum['payload'] = 'ipsum';
+                      $dolores = array();
+                      $dolores['content_type'] = 'text';
+                      $dolores['title'] = 'dolores';
+                      $dolores['payload'] = 'dolores';
+                      $dataResponse['facebook']['text'] = 'A que grupo pertenece?';
+                      $dataResponse['facebook']['quick_replies'] = array();
+                      $dataResponse['facebook']['quick_replies'][] = $lorem;
+                      $dataResponse['facebook']['quick_replies'][] = $ipsum;
+                      $dataResponse['facebook']['quick_replies'][] = $dolores;
+                      break;
+                  default:
+                      $respond = $data["result"]["fulfillment"]["speech"];
+              }
+            }
+              if($data["result"]["actionIncomplete"] != 1)
+              {
+                 //$parametros = $data["result"]["parameters"];
+                 $parametros = $data["result"]["contexts"][4]["parameters"];
+                 User::create([
+                     'name' => $parametros['name'],
+                     'email' => $parametros['email'],
+                     'password' => bcrypt($parametros['password']),
+                     'lastname' => $parametros['lastname'],
+                     'dni' => $parametros['dni'],
+                     'name_apoderado' => $parametros['name_apoderado'],
+                     'lastname_apoderado' => $parametros['lastname_apoderado'],
+                     'dni_apoderado' => $parametros['dni_apoderado'],
+                     'discapacidad' => strtolower($parametros['discapacidad'])
+                 ]);
+                 $respond = 'Listo! Su inscripcion se realizo con exito, recuerde que el circuito comienza el 15 de enero. Puede ingresar a http://52.168.151.74 para ver el nuevo perfil de deportista. Usuario: '.$parametros['email'].' Clave: '.$parametros['password'].'. Gracias';
+              }
+            /*$denuncia = Denuncia::create($atributos);
             if ($denuncia) {
-                /*$tokens = User::where('type_document', 2)->pluck('device')->toArray();
+                $tokens = User::where('type_document', 2)->pluck('device')->toArray();
                 if ($tokens) {
                     $title = 'Robo vehiculo';
                     $msg = $request->placa;
                     $this->pushService->send($tokens, $title, $msg);
-                }*/
-            }
+                }
+            }*/
+            $datos = array();
+            $datos['speech'] = $respond;
+            $datos['source'] = 'caleidos';
+            $datos['data'] = $dataResponse;
+            Log::info(print_r($datos,true));
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
-        $data = $denuncia;
-        return response()->json(compact('data'));
+        //$data = $denuncia;
+        return response()->json(compact('datos'));
     }
 }
